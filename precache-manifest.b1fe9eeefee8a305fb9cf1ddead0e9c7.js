0e72a002e5ead10298ff715c5c1788e111043b5d
Vlad Kiyashko <kiyashko.vlad1@gmail.com>
2020-01-14 01:12:57 +0100

    Updates

---
+++ precache-manifest.b1fe9eeefee8a305fb9cf1ddead0e9c7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d49b573638a093f4661f856b8ec51718",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2124ee7671f5b0e080b",
+    "url": "/static/css/main.a8c23cd6.chunk.css"
+  },
+  {
+    "revision": "a9dfee6d9603d9b4bddd",
+    "url": "/static/js/2.e079e649.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/static/js/2.e079e649.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f2124ee7671f5b0e080b",
+    "url": "/static/js/main.d8a7c5f0.chunk.js"
+  },
+  {
+    "revision": "bc0a437ac56b6d303c33",
+    "url": "/static/js/runtime-main.ca2d27e8.js"
+  },
+  {
+    "revision": "63dfde6852555ff99b43454c806d3bec",
+    "url": "/static/media/header-background1.63dfde68.jpg"
+  },
+  {
+    "revision": "17ae49f41b09ebba30cf041cadf46856",
+    "url": "/static/media/portfolio.17ae49f4.png"
+  },
+  {
+    "revision": "f5acaede890f679b685b33ebd921ad49",
+    "url": "/static/media/portfolio_mobile.f5acaede.png"
+  },
+  {
+    "revision": "9f2c63550123a189f08f44675a167972",
+    "url": "/static/media/smartbrain.9f2c6355.png"
+  },
+  {
+    "revision": "45b1c8855189e068b3b5a8644811d5d3",
+    "url": "/static/media/smartbrain_mobile.45b1c885.png"
+  },
+  {
+    "revision": "557664d79a0b99f358d96f46e467b128",
+    "url": "/static/media/vlad1.557664d7.jpg"
+  }
+]);
